c934ce4b8e4a9edd54f004c15dc4088d05bb7a3a
joao victor gomes <joao-iccz@hotmail.com>
2024-05-28 20:12:48 -0300

    Backup das alterações locais

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-import streamlit as st
-import threading
 import asyncio
+import threading
 import os
 from dotenv import load_dotenv
 from telegram_bot import start_bot as bot_start
@@ -11,16 +10,15 @@ load_dotenv()
 # Obter o token do bot do ambiente
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-st.title("Telegram Bot Status")
-st.write("O bot do Telegram está rodando em segundo plano.")
-
 # Função para iniciar o bot
 async def start_bot_async():
     await bot_start()
 
 # Função para iniciar o bot em uma nova thread
 def start_bot():
-    asyncio.run(start_bot_async())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(start_bot_async())
 
 # Iniciar o bot em uma thread separada
 bot_thread = threading.Thread(target=start_bot)
